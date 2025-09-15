0c524029f9e0cba7bea3d32e3c1846f4f6c08d51
Natal2101 <natavas2101@gmail.com>
2025-09-16 02:06:07 +0700

    lesson6

---
+++ 06_lesson/06_lesson_task_2.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get('http://uitestingplayground.com/textinput')
+
+input_field = driver.find_element(By.CSS_SELECTOR, "input#newButtonName")
+
+input_field.send_keys('"SkyPro"')
+
+driver.find_element(By.CSS_SELECTOR, "button#updatingButton").click()
+
+driver.implicitly_wait(4)
+
+text = driver.find_element(By.CSS_SELECTOR, "button#updatingButton").text
+print(text)
+
+driver.quit()
---
+++ 06_lesson/06_lesson_task_3.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+driver.get(' https://bonigarcia.dev/selenium-webdriver-java/loading-images.html')
+
+waiter = WebDriverWait(driver, 60)
+
+waiter.until(
+    EC.presence_of_all_elements_located((By.CSS_SELECTOR, "img#landscape"))
+)
+
+src_attribute = driver.find_element(By.CSS_SELECTOR, "img#award").get_attribute("src")
+
+print(src_attribute)
+
+driver.quit()
