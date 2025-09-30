85e9ee8137a5fe84fbc879b771978fd22ffe855a
Natal2101 <natavas2101@gmail.com>
2025-09-30 15:29:44 +0700

    lesson8

--- 08_lesson/test_yougile.py
+++ 08_lesson/test_yougile.py
@@ -1,13 +1,10 @@
-import requests
-import json
-
 from YougileApi import YougileApi
 
 
 api = YougileApi("https://ru.yougile.com")
-login = ''
-password = ''
-name = ''
+login = 'natavas2101@gmail.com'
+password = '828405@Natasha'
+name = 'QA_65.0'
 title_project = 'Title Project'
 new_title_project = 'New Title Project'
 users = {}
@@ -15,13 +12,9 @@ users = {}
 
 def test_positive_create_project():
     # 1. получить id компании
-    result = api.get_id_company(login, password, name)
-    id_company = result["content"][0]["id"]
-
+    id_company = api.get_id_company(login, password, name)["content"][0]["id"]
     # 2. получить токен
-    created_key = api.create_key(login, password, id_company)
-    key_company = created_key["key"]
-
+    key_company = api.create_key(login, password, id_company)["key"]
     # 3. создать проект
     created_project = api.create_project(key_company, title_project, users)
     id_project = created_project["id"]
@@ -30,93 +23,58 @@ def test_positive_create_project():
 
 
 def test_negative_create_project():
-    # # 1. получить id компании
-    # id_company = api.get_id_company(login, password, name)
-    #
-    # # 2. получить токен
-    # key_company = api.create_key(login, password, id_company)
+    key_company = 'none-key-company'
+    response = api.create_project(key_company, title_project, users)
 
-    # 3. создать проект
-    key_company = ''
-    id_project = api.create_project(key_company, title_project, users)
-
-    assert id_project is None
+    assert response.get('error')  # проверяем наличие поля "error"
+    assert isinstance(response.get('error'), str)
 
 
 def test_positive_change_title_project():
-    # 1. получить id компании
-    result = api.get_id_company(login, password, name)
-    id_company = result["content"][0]["id"]
+    # 1. создать проект
+    id_company = api.get_id_company(login, password, name)["content"][0]["id"]
+    key_company = api.create_key(login, password, id_company)["key"]
+    id_project = api.create_project(key_company, title_project, users)["id"]
 
-    # 2. получить токен
-    created_key = api.create_key(login, password, id_company)
-    key_company = created_key["key"]
+    # 2. изменить название проекта
+    id_changed_project = api.change_title_project(
+        key_company, new_title_project, users, id_project)["id"]
 
-    # 3. создать проект
-    created_project = api.create_project(key_company, title_project, users)
-    id_project = created_project["id"]
-
-    # 4. изменить название проекта
-    changed_project = api.change_title_project(
-        key_company, new_title_project, users, id_project)
-    id_changed_project = changed_project["id"]
     assert id_changed_project == id_project
 
 
 def test_negative_change_title_project():
-    # 1. получить id компании
-    result = api.get_id_company(login, password, name)
-    id_company = result["content"][0]["id"]
+    id_company = api.get_id_company(login, password, name)["content"][0]["id"]
+    key_company = api.create_key(login, password, id_company)["key"]
 
-    # 2. получить токен
-    created_key = api.create_key(login, password, id_company)
-    key_company = created_key["key"]
-
-    # # 3. создать проект
-    # created_project = api.create_project(key_company, title_project, users)
-    # id_project = created_project["id"]
-
-    # 4. изменить название проекта
-    id_project = ''
-    id_changed_project = api.change_title_project(
+    # изменить название проекта
+    id_project = 'none-project-id'
+    response = api.change_title_project(
          key_company, new_title_project, users, id_project)
-    assert id_changed_project is None
+
+    assert response.get('error')  # проверка наличия поля ошибки
+    assert isinstance(response.get('error'), str)
 
 
 def test_positive_get_title_project():
-    # 1. получить id компании
-    result = api.get_id_company(login, password, name)
-    id_company = result["content"][0]["id"]
+    # 1. создать проект
+    id_company = api.get_id_company(login, password, name)["content"][0]["id"]
+    key_company = api.create_key(login, password, id_company)["key"]
+    id_project = api.create_project(key_company, title_project, users)["id"]
 
-    # 2. получить токен
-    created_key = api.create_key(login, password, id_company)
-    key_company = created_key["key"]
+    # 2. получить информацию о проекте
+    get_title = api.get_title_project(key_company, id_project)['title']
 
-    # 3. создать проект
-    created_project = api.create_project(key_company, title_project, users)
-    id_project = created_project["id"]
-
-    # получить информацию о проекте
-    get_title = api.get_title_project(key_company, id_project)
-    get_title = get_title['title']
     assert get_title == title_project
 
 
 def test_negative_get_title_project():
-    # 1. получить id компании
-    result = api.get_id_company(login, password, name)
-    id_company = result["content"][0]["id"]
-
-    # 2. получить токен
-    created_key = api.create_key(login, password, id_company)
-    key_company = created_key["key"]
-
-    # # 3. создать проект
-    # created_project = api.create_project(key_company, title_project, users)
-    # id_project = created_project["id"]
+    id_company = api.get_id_company(login, password, name)["content"][0]["id"]
+    key_company = api.create_key(login, password, id_company)["key"]
 
     # получить информацию о проекте
-    id_project = ''
-    get_title = api.get_title_project(key_company, id_project)
+    id_project = 'none-project-id'
+    response = api.get_title_project(key_company, id_project)
 
-    assert get_title is None
+    assert response.get('error')  # проверка наличия поля ошибки
+    assert isinstance(response.get('error'), str)
